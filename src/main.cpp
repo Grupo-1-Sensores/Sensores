e6c1cb0959f25c69aede34144eaa646c3de233fb
GAEL DE ROUSSET RASPANTINI SALVADOR <24018834870@info.local>
2026-05-26 09:03:26 -0300

    Versão sem MQTT final

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,11 @@
 #include <DHT.h>
 #include <Bounce2.h>
 
-#define pinoSom 11
 #define TIPODHT DHT22
-#define pinoTemp 2
+#define pinoTemp 5
 
 bool comeco = true;
+const int pinoSom  = 11;
 
 DHT dht(pinoTemp, TIPODHT);
 Bounce botao = Bounce();
@@ -18,55 +18,59 @@ uint contador = 0;
 void setup()
 {
   Serial.begin(115200);
+  dht.begin();
+  pinMode(pinoSom, INPUT);
+  botao.attach(0, INPUT_PULLUP);
 }
 
 void loop()
 {
+  sensores();
+  botao.update();
 }
 void sensores()
 {
-
-  dht.begin();
-  pinMode(pinoSom, INPUT);
-  botao.attach(0, INPUT_PULLUP);
-  botao.update();
-
   bool temVolume = digitalRead(pinoSom);
   int volume = analogRead(pinoSom);
   float umidade = dht.readHumidity();
   float tempC = dht.readTemperature();
   float tempAnterior = tempC;
 
-  if (isnan(umidade) || isnan(tempC) || isnan(temVolume))
+  if(isnan(umidade) || isnan(tempC))
   {
-    Serial.println("Falha ao iniciar o sensores");
-    Serial.println("Verifique a conexão e os pinos definidos");
-    return;
+      Serial.println("Falha ao iniciar o sensor");
+      Serial.println("Verifique a conexão e o pino definido");
+      return;
   }
 
-  if (comeco)
-  {
-    Serial.println("==== Sensor DHT 22 ====");
-    Serial.println("Sensor Iniciado");
-    Serial.println("=======================");
-    Serial.printf("Temperatura: %.1f C\n\r", tempC);
-    Serial.printf("Umidade: %.1f %%\n\r", umidade);
-    Serial.printf("Som: %.1f %%\n\r", volume);
-    comeco = false;
-  }
+   if (comeco)
+   {
+     Serial.println("==== Sensor DHT 22 ====");
+     Serial.println("Sensor Iniciado");
+     Serial.println("=======================");
+     Serial.printf("Temperatura: %.1f C\n\r", tempC);
+     Serial.printf("Umidade: %.1f %%\n\r", umidade);
+     Serial.printf("Som: %d nm\n\r", volume);
+     comeco = false;
+   }
 
   if (millis() - contador > 120000)
   {
+    Serial.println("=======================");
     Serial.printf("Temperatura: %.1f C\n\r", tempC);
     Serial.printf("Umidade: %.1f %%\n\r", umidade);
-    Serial.printf("Som: %.1f %%\n\r", volume);
+    Serial.printf("Som: %d nm \n\r", volume);
+    Serial.println("=======================");
     contador = millis();
   }
 
   if (botao.fell())
   {
+    Serial.println("=======================");
     Serial.printf("Temperatura: %.1f C\n\r", tempC);
     Serial.printf("Umidade: %.1f %%\n\r", umidade);
-    Serial.printf("Som: %.1f %%\n\r", volume);
+    Serial.printf("Som: %d nm\n\r", volume);
+    Serial.println("=======================");
   }
+  
 }
